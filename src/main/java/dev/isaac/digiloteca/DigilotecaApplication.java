3d639ca017bca9b564826dcb74bed84ad09423ba
Isaac Santos <goncalvessantosisaac@gmail.com>
2026-08-25 20:09:51 -0400

    first commit

---
+++ src/main/java/dev/isaac/digiloteca/DigilotecaApplication.java
@@ -0,0 +1,13 @@
+package dev.isaac.digiloteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigilotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigilotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/isaac/digiloteca/DigilotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.isaac.digiloteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigilotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
